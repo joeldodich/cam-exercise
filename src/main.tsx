7e697467d8c98994dc4f238320dfa0d2f5dac3d0
Joel Dodich <88948803+joeldodich@users.noreply.github.com>
2024-06-25 21:51:55 -0500

    add query and loader

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
@@ -7,7 +8,9 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-            <App />
+            <QueryClientProvider client={new QueryClient()}>
+                <App />
+            </QueryClientProvider>
         </ThemeProvider>
     </React.StrictMode>
 );
--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,7 +1,11 @@
 import { PersonAvatar } from "@/components/common/PersonAvatar/PersonAvatar";
 import { ScrollFadeFooter } from "@/components/common/ScrollFadeFooter/ScrollFadeFooter";
-import { TitleCard } from "@/components/common/TitleCard/TitleCard";
+import {
+    TitleCard,
+    TitleCardLoader,
+} from "@/components/common/TitleCard/TitleCard";
 import { TopNav } from "@/components/TopNav/TopNav";
+import { useQuery } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 
 export const Dashboard = () => {
@@ -11,6 +15,13 @@ export const Dashboard = () => {
             <PersonAvatar className="h-4 w-4" /> Created 6/24/24 at 10:24AM
         </span>
     );
+
+    const { isLoading, isSuccess, isFetching } = useQuery({
+        queryKey: ["parts"],
+        queryFn: async () =>
+            await new Promise((resolve) => setTimeout(resolve, 2000)),
+    });
+
     return (
         <div className="h-full w-full overflow-y-auto flex flex-col">
             <TopNav />
@@ -18,101 +29,116 @@ export const Dashboard = () => {
                 <h1 className="text-3xl font-bold text-slate-700 mb-3">
                     Ready for Review
                 </h1>
-                <div className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mx-auto gap-3 align-middle">
-                    <TitleCard
-                        title="Model Viewer"
-                        imageUrl="https://via.placeholder.com/150"
-                        descriptionSlot={descriptionSlotElement}
-                        size="lg"
-                        onClick={() => navigation("1")}
-                    />
-                    <TitleCard
-                        title="Model Viewer"
-                        imageUrl="https://via.placeholder.com/150"
-                        descriptionSlot={descriptionSlotElement}
-                        size="lg"
-                        onClick={() => navigation("2")}
-                    />
-                    <TitleCard
-                        title="Model Viewer"
-                        imageUrl="https://via.placeholder.com/150"
-                        descriptionSlot={descriptionSlotElement}
-                        size="lg"
-                        onClick={() => navigation("3")}
-                    />
-                    <TitleCard
-                        title="Model Viewer"
-                        imageUrl="https://via.placeholder.com/150"
-                        descriptionSlot={descriptionSlotElement}
-                        size="lg"
-                        onClick={() => navigation("3")}
-                    />
-                    <TitleCard
-                        title="Model Viewer"
-                        imageUrl="https://via.placeholder.com/150"
-                        descriptionSlot={descriptionSlotElement}
-                        size="lg"
-                        onClick={() => navigation("3")}
-                    />
-                    <TitleCard
-                        title="Model Viewer"
-                        imageUrl="https://via.placeholder.com/150"
-                        descriptionSlot={descriptionSlotElement}
-                        size="lg"
-                        onClick={() => navigation("3")}
-                    />
-                    <TitleCard
-                        title="Model Viewer"
-                        imageUrl="https://via.placeholder.com/150"
-                        descriptionSlot={descriptionSlotElement}
-                        size="lg"
-                        onClick={() => navigation("3")}
-                    />
-                    <TitleCard
-                        title="Model Viewer"
-                        imageUrl="https://via.placeholder.com/150"
-                        descriptionSlot={descriptionSlotElement}
-                        size="lg"
-                        onClick={() => navigation("3")}
-                    />
-                    <TitleCard
-                        title="Model Viewer"
-                        imageUrl="https://via.placeholder.com/150"
-                        descriptionSlot={descriptionSlotElement}
-                        size="lg"
-                        onClick={() => navigation("3")}
-                    />
-                    <TitleCard
-                        title="Model Viewer"
-                        imageUrl="https://via.placeholder.com/150"
-                        descriptionSlot={descriptionSlotElement}
-                        size="lg"
-                        onClick={() => navigation("3")}
-                    />
-                    <TitleCard
-                        title="Model Viewer"
-                        imageUrl="https://via.placeholder.com/150"
-                        descriptionSlot={descriptionSlotElement}
-                        size="lg"
-                        onClick={() => navigation("3")}
-                    />
-                    <TitleCard
-                        title="Model Viewer"
-                        imageUrl="https://via.placeholder.com/150"
-                        descriptionSlot={descriptionSlotElement}
-                        size="lg"
-                        onClick={() => navigation("3")}
-                    />
-                    <TitleCard
-                        title="Model Viewer"
-                        imageUrl="https://via.placeholder.com/150"
-                        descriptionSlot={descriptionSlotElement}
-                        size="lg"
-                        onClick={() => navigation("3")}
-                    />
-                </div>
+                {isLoading && <DashboardLoader />}
+                {!isLoading && (
+                    <div className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mx-auto gap-3 align-middle">
+                        <TitleCard
+                            title="Model Viewer"
+                            imageUrl="https://via.placeholder.com/150"
+                            descriptionSlot={descriptionSlotElement}
+                            size="lg"
+                            onClick={() => navigation("1")}
+                        />
+                        <TitleCard
+                            title="Model Viewer"
+                            imageUrl="https://via.placeholder.com/150"
+                            descriptionSlot={descriptionSlotElement}
+                            size="lg"
+                            onClick={() => navigation("2")}
+                        />
+                        <TitleCard
+                            title="Model Viewer"
+                            imageUrl="https://via.placeholder.com/150"
+                            descriptionSlot={descriptionSlotElement}
+                            size="lg"
+                            onClick={() => navigation("3")}
+                        />
+                        <TitleCard
+                            title="Model Viewer"
+                            imageUrl="https://via.placeholder.com/150"
+                            descriptionSlot={descriptionSlotElement}
+                            size="lg"
+                            onClick={() => navigation("3")}
+                        />
+                        <TitleCard
+                            title="Model Viewer"
+                            imageUrl="https://via.placeholder.com/150"
+                            descriptionSlot={descriptionSlotElement}
+                            size="lg"
+                            onClick={() => navigation("3")}
+                        />
+                        <TitleCard
+                            title="Model Viewer"
+                            imageUrl="https://via.placeholder.com/150"
+                            descriptionSlot={descriptionSlotElement}
+                            size="lg"
+                            onClick={() => navigation("3")}
+                        />
+                        <TitleCard
+                            title="Model Viewer"
+                            imageUrl="https://via.placeholder.com/150"
+                            descriptionSlot={descriptionSlotElement}
+                            size="lg"
+                            onClick={() => navigation("3")}
+                        />
+                        <TitleCard
+                            title="Model Viewer"
+                            imageUrl="https://via.placeholder.com/150"
+                            descriptionSlot={descriptionSlotElement}
+                            size="lg"
+                            onClick={() => navigation("3")}
+                        />
+                        <TitleCard
+                            title="Model Viewer"
+                            imageUrl="https://via.placeholder.com/150"
+                            descriptionSlot={descriptionSlotElement}
+                            size="lg"
+                            onClick={() => navigation("3")}
+                        />
+                        <TitleCard
+                            title="Model Viewer"
+                            imageUrl="https://via.placeholder.com/150"
+                            descriptionSlot={descriptionSlotElement}
+                            size="lg"
+                            onClick={() => navigation("3")}
+                        />
+                        <TitleCard
+                            title="Model Viewer"
+                            imageUrl="https://via.placeholder.com/150"
+                            descriptionSlot={descriptionSlotElement}
+                            size="lg"
+                            onClick={() => navigation("3")}
+                        />
+                        <TitleCard
+                            title="Model Viewer"
+                            imageUrl="https://via.placeholder.com/150"
+                            descriptionSlot={descriptionSlotElement}
+                            size="lg"
+                            onClick={() => navigation("3")}
+                        />
+                        <TitleCard
+                            title="Model Viewer"
+                            imageUrl="https://via.placeholder.com/150"
+                            descriptionSlot={descriptionSlotElement}
+                            size="lg"
+                            onClick={() => navigation("3")}
+                        />
+                    </div>
+                )}
             </div>
             <ScrollFadeFooter />
+        </div>
+    );
+};
+
+const DashboardLoader = () => {
+    const loaderCount = 24;
+    const range = Array.from(Array(loaderCount).keys());
+    return (
+        <div className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mx-auto gap-3 align-middle">
+            {range.map((index) => (
+                <TitleCardLoader key={index} size="lg" />
+            ))}
         </div>
     );
 };
